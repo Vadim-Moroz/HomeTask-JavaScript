72cd54010eae62603230a560ac24bd71ccb9aaca
Vadim-Moroz <morozvadim1804@gmail.com>
2026-07-19 14:52:30 +0300

    Виконано завдання 1.1, 1.2, 1.3, 1.4

---
+++ hw1/task-2/main2.js
@@ -0,0 +1,4 @@
+let firstName = "Moroz";
+let middleName = "Vadym";
+let lastName = "Volodymyrovych";
+let person = firstName +''+ middleName +''+ lastName;
---
+++ hw1/task-4/main4.js
@@ -0,0 +1,7 @@
+let name = prompt("Введіть ваше ім'я:","Vadym");
+let lastname = prompt("Введіть ваше по батькові:","Volodymyrovych");
+let age = Number(prompt("Введіть ваш вік:",20));
+
+console.log(name);
+console.log(lastname);
+console.log(age);
