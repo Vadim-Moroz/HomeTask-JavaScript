48898028013a735c5f868666a32f5d38c636d549
Vadim-Moroz <morozvadim1804@gmail.com>
2026-07-29 14:01:16 +0300

    Виконано завдання 5.1

---
+++ hw5/task-1/main.js
@@ -0,0 +1,10 @@
+let exchange = (sumUAH,currencyValues,exchangeCurrency) =>{
+    let result = 0;
+    for(const val of currencyValues) {
+        if (val.currency === exchangeCurrency ) {
+            result = sumUAH / val['value'];
+        }
+    }
+    return result;
+}
+console.log(exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD'))
